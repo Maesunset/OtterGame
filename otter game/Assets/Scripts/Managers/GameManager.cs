d9562b32b963bca831e71778803cbe1a9ad48589
Jared Lares <119090809+JaredLares@users.noreply.github.com>
2026-05-08 13:32:46 -0600

    select material update

--- otter game/Assets/Scripts/Managers/GameManager.cs
+++ otter game/Assets/Scripts/Managers/GameManager.cs
@@ -1,10 +1,11 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-   [SerializeField] private int materialSelected;
-    
     public static GameManager Instance;
+    private Dictionary<int, int> scaleValues = new Dictionary<int, int>();
+
     private void Awake()
     {
         if (Instance == null)
@@ -18,17 +19,46 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void ActualMaterial(int materialID)
+    public void LoadMaterial(int MaterialID)
     {
-        if (materialID == materialSelected)
+        if(scaleValues.TryGetValue(MaterialID, out var value))
         {
-            materialSelected = 0;
-            Debug.Log("unselect Material");
+            value++;
+            scaleValues[MaterialID] = value;
+            Debug.Log("new new value of material:");
         }
         else
         {
-            materialSelected = materialID;
-            Debug.Log("select new  Material");
+            scaleValues[MaterialID] = 1;
+            Debug.Log("add new material to scale");
         }
+    }
+
+    public void UnloadMaterial(int MaterialID)
+    {
+        if (scaleValues.TryGetValue(MaterialID, out var value))
+        {
+            value--;
+            if(value == 0)
+            {
+                scaleValues.Remove(MaterialID);
+                Debug.Log("material removed");
+            }
+            else
+            {
+                scaleValues[MaterialID] = value;
+                Debug.Log(" new value of material: " + value);
+            }
+        }
+        else
+        {
+            Debug.Log("no material ");
+        }
+
+    }
+    public void ResetDictionary()
+    {
+        scaleValues = new Dictionary<int, int>();
+        
     }
 }
--- otter game/Assets/Scripts/Managers/SelectUI.cs
+++ otter game/Assets/Scripts/Managers/SelectUI.cs
@@ -11,6 +11,7 @@ public class SelectUI : MonoBehaviour
 
     void Update()
     { 
+        // click Derecho
         if (Input.GetMouseButtonDown(0))
         {
             PointerEventData pointerData = new PointerEventData(EventSystem.current);
@@ -27,7 +28,31 @@ public class SelectUI : MonoBehaviour
                     {
                         if (results[i].gameObject.GetComponent<hotbarItem>())
                         {
-                            GameManager.Instance.ActualMaterial(results[i].gameObject.GetComponent<hotbarItem>().GetID());
+                            GameManager.Instance.LoadMaterial(results[i].gameObject.GetComponent<hotbarItem>().GetID());
+                        }
+                    }
+                }
+            }
+        }
+        // click izquierdo
+        if (Input.GetMouseButtonDown(1))
+        {
+            PointerEventData pointerData = new PointerEventData(EventSystem.current);
+            pointerData.position = Input.mousePosition;
+
+            List<RaycastResult> results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(pointerData, results);
+
+            if (results.Count > 0)
+            {
+                for(int i = 0;i < results.Count;i++)
+                {
+                    if (results[i].gameObject.CompareTag(tagToSelect))
+                    {
+                        if (results[i].gameObject.GetComponent<hotbarItem>())
+                        {
+                            GameManager.Instance.UnloadMaterial(results[i].gameObject.GetComponent<hotbarItem>().GetID());
+
                         }
                     }
                 }
