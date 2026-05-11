c21a50c8733fba3b008a85cfdd7a75d7c0eaa092
JaredLares <119090809+JaredLares@users.noreply.github.com>
2026-05-11 09:54:37 -0600

    commit

---
+++ otter game/Assets/Scripts/animals/BaseAnimalPReferences.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BaseAnimalPReferences", menuName = "Scriptable Objects/BaseAnimalPReferences")]
+public class BaseAnimalPReferences : ScriptableObject
+{
+    
+}
