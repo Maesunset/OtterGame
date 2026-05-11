d07fb24755150aa2176d2ac97fbed703612488ed
JaredLares <119090809+JaredLares@users.noreply.github.com>
2026-05-11 11:38:17 -0600

    commit

--- otter game/Assets/Scripts/animals/AnimalManaer.cs
+++ otter game/Assets/Scripts/animals/AnimalManaer.cs
@@ -3,4 +3,6 @@ using UnityEngine;
 public class AnimalManaer : MonoBehaviour
 {
     [SerializeField] private BaseAnimalPReferences preferences;
+    [SerializeField] private Animator animalAnim;
+    
 }
